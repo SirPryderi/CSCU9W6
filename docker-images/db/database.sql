03010f0fead88d97c646c11d9e4071c1ace1ee3a
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-03-23 19:46:32 +0000

    Make database field not-null

--- docker-images/db/database.sql
+++ docker-images/db/database.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users;
 create table users
 (
   id       serial primary key,
-  email    varchar(50) unique,
-  password varchar(128),
-  salt     varchar(128)
+  email    varchar(50) unique not null,
+  password varchar(128)       not null,
+  salt     varchar(128)       not null
 );
