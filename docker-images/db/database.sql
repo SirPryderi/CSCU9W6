86eb5e08912fe6c0656621e6165920368d77648d
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-03-23 13:27:14 +0000

    Initialised docker environment

---
+++ docker-images/db/database.sql
@@ -0,0 +1,8 @@
+create table my_table
+(
+  id          integer,
+  description varchar
+);
+
+INSERT INTO my_table (id, description) VALUES (0, 'it works');
+INSERT INTO my_table (id, description) VALUES (0, 'it is happy');
---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vittorio
+ * Date: 19/03/18
+ * Time: 18:31
+ */
+
+echo "<h1>It works!</h1><pre>";
+
+$db = new PDO('pgsql:dbname=database;host=db;port=5432;user=root;password=supersafepassword');
+
+$result = $db->query("SELECT * FROM my_table");
+
+print_r($result->fetch());
