7707586a2560966704ddda9586dc509c23f49ae4
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-03-23 16:58:55 +0000

    Format sql

--- docker-images/db/database.sql
+++ docker-images/db/database.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS users;
 
 create table users
 (
-  id       serial primary key ,
-  email    varchar(50) unique ,
+  id       serial primary key,
+  email    varchar(50) unique,
   password varchar(128),
   salt     varchar(128)
 );
