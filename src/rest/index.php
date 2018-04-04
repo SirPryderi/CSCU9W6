34eb99d4004c7ceb34cef3811b2a1cbaf927b1c3
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-04-04 15:07:42 +0100

    Update restful api to support checking for existing user

--- src/rest/index.php
+++ src/rest/index.php
@@ -12,15 +12,23 @@ use PHP\User;
 include '../vendor/autoload.php';
 
 $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+$return = ["status" => "success"];
+$redirecting = isset($_POST['redirect']);
 
 User::connectDb();
 
 try {
     handleAction($action);
-    handleRedirect();
+
+    $redirecting = handleRedirect();
 } catch (Exception $e) {
     Notifier::addErrorMessage($e->getMessage());
-    goBack();
+
+    if ($redirecting) goBack();
+}
+
+if (!$redirecting) {
+    handleJsonResponse($return);
 }
 
 // END OF ACTIONS
@@ -41,14 +49,18 @@ try {
  */
 function handleAction($action): void
 {
+    global $return;
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 
     switch ($action) {
         case 'register':
-            User::createUser($email, $_POST['password'], $_POST['password-confirm']);
+            $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_EMAIL);
+            $passwordConfirm = filter_input(INPUT_POST, 'password-confirm', FILTER_SANITIZE_EMAIL);
+            User::createUser($email, $password, $passwordConfirm);
             break;
         case 'login':
-            User::login($email, $_POST['password']);
+            $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_EMAIL);
+            User::login($email, $password);
             break;
         case 'logout':
             User::logout();
@@ -56,22 +68,47 @@ function handleAction($action): void
         case 'forgot':
             User::sendNewPasswordByEmail($email);
             break;
+        case 'user-exists':
+            $return["user-exists"] = (User::getUserByEmail($email) != null);
+            break;
         default:
-            new Exception("Invalid request.");
+            new RuntimeException("Invalid request.");
     }
 }
 
 
-function handleRedirect(): void
+function handleRedirect(): bool
 {
     $redirect = filter_input(INPUT_POST, 'redirect', FILTER_SANITIZE_STRING);
 
     if ($redirect) {
         header("Location: $redirect");
+        return true;
     }
+
+    return false;
+}
+
+/**
+ * @param $return
+ */
+function handleJsonResponse($return): void
+{
+    header('Content-type: application/json');
+
+    $message = Notifier::getErrorMessage();
+
+    if (!empty($message)) {
+        $return["status"] = "error";
+        $return["message"] = $message;
+    }
+
+    echo json_encode($return);
 }
 
 function goBack(): void
 {
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    if (isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER'])) {
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    }
 }
